bd32ea728a102474c2b78ffea940429e1d49d793
Your Name <youremail@gmail.com>
2025-12-17 04:58:03 +0530

    Fix syntax error in settings.py middleware

--- mamis_premium/settings.py
+++ mamis_premium/settings.py
@@ -38,9 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'shop',  # <--- ADD THIS LINE (Don't forget the comma!)
 ]
-
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware whitenoise.middleware.WhiteNoiseMiddleware',
+    'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -48,7 +48,6 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
-
 ROOT_URLCONF = 'mamis_premium.urls'
 
 TEMPLATES = [
