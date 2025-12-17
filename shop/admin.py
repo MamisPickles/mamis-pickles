9cc0f5689d00b1177639248622f2b18c3b636532
Your Name <youremail@gmail.com>
2025-12-17 06:16:42 +0530

    Updated top banner image

--- shop/admin.py
+++ shop/admin.py
@@ -1,9 +1,22 @@
 from django.contrib import admin
+from .models import Category, Product, ProductImage
 
-# Register your models here.
-from django.contrib import admin
-from .models import Category, Product
+# 1. CREATE THE GALLERY INLINE
+# This allows uploading extra images inside the Product page
+class ProductImageInline(admin.TabularInline):
+    model = ProductImage
+    extra = 1  # Shows 1 empty slot by default (click "Add" for more)
 
-# This tells Django: "Show these tables on the Admin Page"
+# 2. CUSTOMIZE THE PRODUCT ADMIN
+class ProductAdmin(admin.ModelAdmin):
+    # Add the gallery inline here
+    inlines = [ProductImageInline]
+    
+    # Optional: Shows these columns in the product list
+    list_display = ('name', 'price', 'category', 'is_available')
+
+# 3. REGISTER MODELS
 admin.site.register(Category)
-admin.site.register(Product)
+admin.site.register(Product, ProductAdmin)
+# You can also register ProductImage separately if you want to see them as a list
+admin.site.register(ProductImage)
